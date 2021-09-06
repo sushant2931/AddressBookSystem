0abefecf8f4d160295574a3562d017cf431eb087
sushant2931 <sushantlad8888@gmail.com>
2021-09-06 23:50:36 +0530

    Edit Contact

--- AddressBook.java
+++ AddressBook.java
@@ -1,5 +1,7 @@
 package com.bl.addressbooksystem;
 
+import java.util.*;
+
 public class AddressBook extends Contact {
 	String address, city, state;
 	int zip;
@@ -45,4 +47,37 @@ public class AddressBook extends Contact {
 		return zip;
 	}
 
+	public void showContact(AddressBook contact) {
+
+		System.out.print("\n-----------------");
+		System.out.print("\nFirst Name  : " + contact.getFirstName());
+		System.out.print("\nLast Name   : " + contact.getLastName());
+		System.out.print("\nAddress     : " + contact.getAddress());
+		System.out.print("\nCity        : " + contact.getCity());
+		System.out.print("\nState       : " + contact.getState());
+		System.out.print("\nPone Number : " + contact.getPhoneNo());
+		System.out.print("\nE-mail      : " + contact.getEmail());
+		System.out.print("\nZip         : " + contact.getZip());
+	}
+
+	public void editContact(AddressBook[] contactArr) {
+
+		Scanner sc = new Scanner(System.in);
+		System.out.print("\nEnter the First name for update Address : ");
+		String editName = sc.nextLine();
+		int flag = 1;
+		for (int i = 0; i < contactArr.length; i++) {
+			if (editName.equals(contactArr[i].getFirstName())) {
+				System.out.print("\nEnter your new Address : ");
+				address = sc.nextLine();
+				contactArr[i].setAddress(address);
+				flag = 0;
+			}
+		}
+		if (flag == 1) {
+			System.out.print("\ncouldn't find the contact..");
+		}
+		sc.close();
+
+	}
 }
