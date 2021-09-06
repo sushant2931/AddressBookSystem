0d55e95a3bbf48287f628975fcd08870a3040b77
sushant2931 <sushantlad8888@gmail.com>
2021-09-06 23:14:03 +0530

    Welcome On Master Branch

---
+++ AddressBook.java
@@ -0,0 +1,5 @@
+package com.bl.addressbooksystem;
+
+public class AddressBook {
+
+}
---
+++ AddressBookMain.java
@@ -0,0 +1,7 @@
+package com.bl.addressbooksystem;
+
+public class AddressBookMain {
+	public static void main(String[] args) {
+		System.out.println("Welcome In Adress Book Program On Master Branch");
+	}
+}
