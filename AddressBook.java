2870d6c32ec1d4a59c72dcb70e2bd643f0174754
sushant2931 <sushantlad8888@gmail.com>
2021-09-06 23:24:04 +0530

    Create A contact In Address Book

--- AddressBook.java
+++ AddressBook.java
@@ -1,5 +1,21 @@
 package com.bl.addressbooksystem;
 
 public class AddressBook {
+	String firstName, lastName, address, city, state, phoneNo, email;
+    long zip;
+    public void setDetails(){
 
+        firstName = "Sushant";
+        lastName = "Lad";
+        address = "BalajiNagar";
+        city ="Pune";
+        state = "Maharashtra";
+        zip = 411043;
+        phoneNo = "8830602356";
+        email = "sushantlad8888@gmail.com";
+    }
+    public void printValue(){
+
+        System.out.println("First Nmae : " + firstName  + "\nLast Name : " + lastName + "\nAddress : " + address + "\nCity : " + city + "\nState : " + state + "\nZip : " + zip + "\nPhone Number : " + phoneNo + "\nE-mail : " + email); 
+    }
 }
--- AddressBookMain.java
+++ AddressBookMain.java
@@ -3,5 +3,10 @@ package com.bl.addressbooksystem;
 public class AddressBookMain {
 	public static void main(String[] args) {
 		System.out.println("Welcome In Adress Book Program On Master Branch");
+
+		AddressBook contact = new AddressBook();
+		contact.setDetails();
+		contact.printValue();
+
 	}
 }
